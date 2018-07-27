55bed6a868923bd34e922d621d694058e2eafc7f
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-26 23:47:30 -0300

    a17 Numbe: alguns cuidados

---
+++ s03/exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,5 @@
+console.log(7 / 0) // Infinity
+console.log("10" / 2) // 5
+console.log("a" * 2) // NaN
+console.log(0.6 + 1.1) // Imprecisão
+console.log((1.909).toFixed(1)) // Não precisou declarar uma variável para usar o método
