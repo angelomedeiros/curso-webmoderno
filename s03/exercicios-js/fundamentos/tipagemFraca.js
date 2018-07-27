b4706ee7ced61cee39bb3f4b9746eddc3d1e34ee
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-26 23:16:29 -0300

    a15 Tipagem fraca

---
+++ s03/exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,23 @@
+/* 
+* O js apresenta inferência de tipo, que é a capacidade do interpretador 
+* não requerer o tipo da variável (js, python e php)
+*/
+
+/*
+* A tipagem ser fraca, significa dizer que não a verficação de tipo em 
+* tempo de execução ou compilação ou seja, é possível fazer coisas
+* como 1 + '12' -> '112' (js, php) (relação com valor)
+*/
+
+/*
+* js é também uma tipagem dinâmica, ou seja, é possível alterar o tipo 
+* de uma variável em tempo de execução (js, python) (relação com variável)
+*/
+
+let x = 'Angelo'
+console.log(x)
+console.log(typeof x)
+
+x = 3.141592
+console.log(x)
+console.log(typeof x)
