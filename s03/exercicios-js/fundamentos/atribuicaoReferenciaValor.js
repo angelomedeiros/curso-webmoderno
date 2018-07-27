f1b023469d69e8ce9771489dd2d3c076df5a0337
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-27 02:25:56 -0300

    a24 Entendendo o Null & Undefined

---
+++ s03/exercicios-js/fundamentos/atribuicaoReferenciaValor.js
@@ -0,0 +1,8 @@
+/**
+ * Quando é feita uma atribuição com valores primitivos, ocorre uma atribuição 
+ * por valor;
+ * 
+ * Quando a atribuição é feita com objeto, ocorre uma atribuição por referência;
+ * 
+ */
+
---
+++ s03/exercicios-js/fundamentos/nullAndUndefined.js
@@ -0,0 +1,5 @@
+/**
+ * Não atribuir o undefined durante o desnvolvimento,
+ * recomendado utilizar o null quando quiser indicar que um 
+ * atributo não existe mais na memória.
+ */
