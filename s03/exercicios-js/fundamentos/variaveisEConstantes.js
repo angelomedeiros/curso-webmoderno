59e2fd58d98dda248fd6dc0378e8c73d5adcbe13
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-26 22:25:47 -0300

    a14 O basico de var, let e const

---
+++ s03/exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,15 @@
+var a = 1
+let b = 2
+const c = 3
+
+// Var permimite redeclarar uma variável
+var a = 10
+
+// Let não permite redeclarar uma variável
+// let b = 20  // Obtem erro
+b = 20
+
+console.log(a, b)
+
+// Const não permite alterar o valor da constante, obviamente :/
+// c = 3
