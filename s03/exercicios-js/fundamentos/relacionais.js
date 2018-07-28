13f2bd8d6b1150706c6568333f5f6d2d63a494a0
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-28 03:43:09 -0300

    a45 Operadores Relacionais

---
+++ s03/exercicios-js/fundamentos/relacionais.js
@@ -0,0 +1,8 @@
+// Todos retornam true
+console.log('1' == 1)
+console.log('1' == true)
+console.log('' == false)
+console.log(0 == false)
+console.log(null == undefined) 
+console.log(null != false)
+console.log(undefined != false)
